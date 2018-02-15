b882a8082922212dc4d1be1b33ead1274fd760be
Felice Pantaleo <felixpantaleo@gmail.com>
2018-02-15 17:12:52 +0100

    git ignore gitbook folder

--- update_website.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd ~/work/github-myrepos/patatrack-website && git pull && bundle exec jekyll build && scp -r _site/* fpantale@lxplus.cern.ch:/eos/user/f/fpantale/www/patatrack-website/
